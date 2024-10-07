3c40e05d04bf165e75cc0bd1cae9343fe903470e
nsir adem <nsir.adem@esprit.tn>
2024-10-07 22:59:00 +0100

    3 commit

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,31 +5,29 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { NextAuthOptions } from "next-auth";
 
+
 export const authConfig: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      
     }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
+ 
   ],
   pages: {
-    signIn: "/", // Remplace "/signin" par "/" ou une autre page personnalisée
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // Utiliser baseUrl si aucun `url` n'est spécifié
-      return baseUrl;
-    },
+    signIn: "/signin",
   },
 };
+
 
 export async function loginIsRequiredServer() {
   const session = await getServerSession(authConfig);
   if (!session) {
-    redirect("/");
+    redirect("/signin");
   }
 }
