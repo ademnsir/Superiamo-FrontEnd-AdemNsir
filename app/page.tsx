9a2205469a79986f07ea8732839a9b6225f1e175
nsir adem <nsir.adem@esprit.tn>
2024-10-07 02:24:12 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function SignInPage() {
   //consomation
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://superiamo-backend-ademnsir-production.up.railway.app/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -25,7 +25,7 @@ export default function SignUpPage() {
   //consomation
   const handleSubmit = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://superiamo-backend-ademnsir-production.up.railway.app/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
