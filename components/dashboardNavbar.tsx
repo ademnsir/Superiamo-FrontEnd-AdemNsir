99ecdbff43b205bac410f1ed2d0c2fa5fa0a1898
nsir adem <nsir.adem@esprit.tn>
2024-10-09 18:59:19 +0100

    7 commit

--- components/dashboardNavbar.tsx
+++ components/dashboardNavbar.tsx
@@ -5,7 +5,7 @@ import { FaCog } from "react-icons/fa";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import bgprofile from "@/public/profile.png";
+import bgprofile from "@/public/bgimage.jpg";
 
 function DashboardNavbar() {
   const { data: session, status } = useSession();
