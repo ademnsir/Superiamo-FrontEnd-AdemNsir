6d5e7ebf4b60fc28bf4e1e0e83c14902bbd76bff
nsir adem <nsir.adem@esprit.tn>
2024-10-09 18:13:34 +0100

    chnage

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import illustration from "@/public/signup.jpg";
 
+
 interface FormErrors {
   nom?: string;
   prenom?: string;
