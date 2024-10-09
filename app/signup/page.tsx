590a366b01303e38c84c2a796828ab2f997814e0
nsir adem <nsir.adem@esprit.tn>
2024-10-09 18:19:50 +0100

    chnage

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import illustration from "@/public/signup.jpg";
